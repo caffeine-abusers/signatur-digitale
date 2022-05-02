eeb0f6658275a5d8177d2fec8313eb40fe385d8e
Marek Charvát <85548309+charviis@users.noreply.github.com>
2022-05-02 13:14:43 +0200

    Create FileUtils

---
+++ src/main/java/eu/caffeineabusers/signaturdigitale/utils/file/FileUtils.java
@@ -0,0 +1,35 @@
+package eu.caffeineabusers.signaturdigitale.utils.file;
+
+import lombok.experimental.UtilityClass;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.FileReader;
+
+/**
+ * Utility class for file operations.
+ *
+ * @author Tomáš Plánský
+ */
+@UtilityClass
+public final class FileUtils {
+
+    /**
+     * Reads the file and returns its content as a string.
+     *
+     * @param file The file to read.
+     * @return The content of the file as a string.
+     */
+    @NotNull
+    public static String readFile(@NotNull File file) throws Exception {
+        StringBuilder content = new StringBuilder();
+        FileReader fr = new FileReader(file);
+        int c;
+        while ((c = fr.read()) != -1) {
+            content.append((char) c);
+        }
+        fr.close();
+        return content.toString();
+    }
+
+}
