307ec4b16e19bf1c5a9995e5901a2812ab12d11a
tomas-plansky <plansky.tomas@seznam.cz>
2022-05-05 08:25:58 +0200

    Create CipherUtils.java

---
+++ src/main/java/eu/caffeineabusers/signaturdigitale/utils/CipherUtils.java
@@ -0,0 +1,14 @@
+package eu.caffeineabusers.signaturdigitale.utils;
+
+import lombok.experimental.UtilityClass;
+import org.jetbrains.annotations.NotNull;
+
+@UtilityClass
+public final class CipherUtils {
+
+    public static String encipher(@NotNull String string, @NotNull String key) {
+
+        return null;
+    }
+
+}
