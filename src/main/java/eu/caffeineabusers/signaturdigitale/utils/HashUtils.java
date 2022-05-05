7b0d75bc0ceab09a172a0b0783d8697850a3ea71
tomas-plansky <plansky.tomas@seznam.cz>
2022-05-05 08:25:36 +0200

    Create HashUtils.java

---
+++ src/main/java/eu/caffeineabusers/signaturdigitale/utils/HashUtils.java
@@ -0,0 +1,36 @@
+package eu.caffeineabusers.signaturdigitale.utils;
+
+import lombok.experimental.UtilityClass;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Utility class with some methods related to hashing.
+ *
+ * @author Tomáš Plánský
+ */
+@UtilityClass
+public final class HashUtils {
+
+    /**
+     * Hashes the given string using MD_5.
+     *
+     * @param string The string to hash.
+     * @return The hashed string.
+     */
+    @Contract("_ -> new")
+    public static @NotNull String hash(@NotNull String string) {
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("MD_5");
+            byte[] hashBytes = messageDigest.digest(string.getBytes(StandardCharsets.UTF_8));
+            return new String(hashBytes);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
